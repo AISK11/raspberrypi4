6a1e0a49348b2f37bf7f2b145409b16b71f6c7cf
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-06-17 19:03:50 +0200

    Create i3blocks_date.sh

---
+++ dotfiles/.config/i3/scripts/i3blocks_date.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+ 
+# Author: AISK11
+# Description: This script shows current date and time with timezone in the end.
+# Created for: i3blocks
+ 
+echo "$(date '+%Y-%m-%d %H:%M:%S') $(cat /etc/timezone)"
