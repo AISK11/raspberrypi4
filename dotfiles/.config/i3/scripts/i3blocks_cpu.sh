dd8a40aab3243c85b2255337ee4d3cc6a8b26efa
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-06-17 21:31:39 +0200

    Update i3blocks_cpu.sh

--- dotfiles/.config/i3/scripts/i3blocks_cpu.sh
+++ dotfiles/.config/i3/scripts/i3blocks_cpu.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # Author: AISK11
 # Description: This script shows CPU utilization in percentage.
@@ -9,6 +9,27 @@ USAGE=$((10000 - $(mpstat 1 1 | tail -n 1 | rev | cut -d' ' -f1 | rev | tr -d .)
 USAGE1=$((${USAGE} / 100))
 USAGE2=$((${USAGE} % 100))
 
-printf "%3d.%02d%%\n" ${USAGE1} ${USAGE2}
+# Add leading space in front of variable:
+if [ ${#USAGE1} -eq 1 ]; then
+    USAGE1="  ${USAGE1}"
+elif [ ${#USAGE1} -eq 2 ]; then
+    USAGE1=" ${USAGE1}";
+fi
+
+if [ ${#USAGE2} -eq 1 ]; then
+    USAGE1=" ${USAGE1}"
+fi
+
+echo "${USAGE1}.${USAGE2}%"
+echo "${USAGE1}.${USAGE2}%"
+
+# Color
+if [[ ${USAGE1} -le 33 ]]; then
+    echo "#4BFF57"
+elif [[ ${USAGE1} -le 66 ]]; then
+    echo "#FF994A"
+else
+    echo "#FF4A4A"
+fi
 
 # grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'
