baaf568299daacf1560be81e216d90df5f3b38fe
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-06-17 22:42:08 +0200

    Update i3blocks_temp.sh

--- dotfiles/.config/i3/scripts/i3blocks_temp.sh
+++ dotfiles/.config/i3/scripts/i3blocks_temp.sh
@@ -19,12 +19,10 @@ echo "CPU ${CPU_TEMP_FULL}; BRD ${BRD_TEMP_FULL}"
 # Color
 if [ ${CPU_TEMP} -lt 50 ] && [ ${BRD_TEMP} -lt 50 ]; then
     echo "#4BFF57"
-elif [ ${CPU_TEMP} -lt 65 ] && [ ${BRD_TEMP} -lt 65 ]; then
-    echo "#FFFF4A"  
 elif [ ${CPU_TEMP} -lt 80 ] && [ ${BRD_TEMP} -lt 80 ]; then
-    echo "#FF994A"
+    echo "#FFFF4A"  
 elif [ ${CPU_TEMP} -lt 85 ] && [ ${BRD_TEMP} -lt 85 ]; then
-    echo "FFFF4A"
+    echo "#FF994A"
 else
     echo "#FF4A4A"
 fi
