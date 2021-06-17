d14ad237c859f7cf105908ad4ff5f66aaecffbd6
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-06-17 22:32:52 +0200

    Update i3blocks_temp.sh

--- dotfiles/.config/i3/scripts/i3blocks_temp.sh
+++ dotfiles/.config/i3/scripts/i3blocks_temp.sh
@@ -7,7 +7,22 @@
 # Installation:
 # sudo apt install lm-sensors
 
-BOARD_TEMP=$(vcgencmd measure_temp | egrep -o '[0-9][0-9]\.[0-9]')
-CPU_TEMP=$(sensors | grep temp1 | egrep -o '[0-9][0-9]\.[0-9]')
+BRD_TEMP_FULL=$(vcgencmd measure_temp | egrep -o '[0-9][0-9]\.[0-9]')
+CPU_TEMP_FULL=$(sensors | grep temp1 | egrep -o '[0-9][0-9]\.[0-9]')
 
-echo "CPU ${CPU_TEMP}; BRD ${BOARD_TEMP}"
+BRD_TEMP=${BRD_TEMP_FULL:0:2}
+CPU_TEMP=${CPU_TEMP_FULL:0:2}
+
+echo "CPU ${CPU_TEMP_FULL}; BRD ${BRD_TEMP_FULL}"
+echo "CPU ${CPU_TEMP_FULL}; BRD ${BRD_TEMP_FULL}"
+
+# Color
+if [ ${CPU_TEMP} -lt 50 ] && [ ${BRD_TEMP} -lt 50 ]; then
+    echo "#4BFF57"
+elif [ ${CPU_TEMP} -lt 80 ] && [ ${BRD_TEMP} -lt 80 ]; then
+    echo "#FF994A"
+elif [ ${CPU_TEMP} -lt 85 ] && [ ${BRD_TEMP} -lt 85 ]; then
+    echo "FFFF4A"
+else
+    echo "#FF4A4A"
+fi
