3de6a8910ab8a6147f5f5b2da83efd08689d9efd
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-06-24 20:12:43 +0200

    Update cpu_monitoring.sh

--- scripts/cpu_monitoring.sh
+++ scripts/cpu_monitoring.sh
@@ -3,19 +3,19 @@
 # send alarm, if CPU is above ${ALERT_VALUE}% ${TIME_COUNT} times when measured, measure interval = ${TIME_INTERVAL} seconds.
 # example: send alarm, if CPU is above 90% 5 times when measured, measure interval = 60 seconds
 
-# customize vlaues:
+# customize values
 ALERT_VALUE=0
 TIME_INTERVAL=1
 TIME_COUNT=5
 AFTER_ALARM_SLEEP=10
 LOG_FILE="/var/log/alarms.log"
 
-# System variable, do not edit!
+# System variables, do not edit!
 STRIKES=0
 CPU_HISTORY=()
 
 while true; do
-    # get CPU percentage usage:
+    # get CPU percentage usage
     CPU_USAGE=$((10000 - $(mpstat 1 1 | tail -n 1 | rev | cut -d' ' -f1 | rev | tr -d .)))
     CPU_USAGE_PERCENTILE=$((${CPU_USAGE} / 100))
           
@@ -29,8 +29,9 @@ while true; do
         
     # if strikes are equal (or greater) than TIME_COUNT, append alarm to FILE
     if [[ ${STRIKES} -ge ${TIME_COUNT} ]]; then
-        STRIKES=0
         echo "$(date '+%Y-%m-%d %H:%M:%S') - ALERT! - CPU USAGE is above ${ALERT_VALUE}, measured every ${TIME_INTERVAL} seconds. Values: ${CPU_HISTORY}" >> ${LOG_FILE}        STRIKES=0
+        # nullify strikes and wait AFTER_ALARM_SLEEP seconds, to avoid spam
+        STRIKES=0
         sleep ${AFTER_ALARM_SLEEP}
     fi      
 
