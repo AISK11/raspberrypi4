0806752a8750773dbea18aa49106ffc00b8c3854
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-06-24 20:39:18 +0200

    Update cpu_monitoring.sh

--- scripts/cpu_monitoring.sh
+++ scripts/cpu_monitoring.sh
@@ -4,7 +4,7 @@
 # example: send alarm, if CPU is above 90% 5 times when measured, measure interval = 60 seconds
 
 # customize values
-ALERT_VALUE=0
+ALERT_VALUE=1
 TIME_INTERVAL=1
 TIME_COUNT=5
 AFTER_ALARM_SLEEP=10
@@ -14,26 +14,40 @@ LOG_FILE="/var/log/alarms.log"
 STRIKES=0
 CPU_HISTORY=()
 
+# First run
+CPU_USAGE=$((10000 - $(mpstat 1 1 | tail -n 1 | rev | cut -d' ' -f1 | rev | tr -d .)))
+sleep 1
+
 while true; do
     # get CPU percentage usage
     CPU_USAGE=$((10000 - $(mpstat 1 1 | tail -n 1 | rev | cut -d' ' -f1 | rev | tr -d .)))
     CPU_USAGE_PERCENTILE=$((${CPU_USAGE} / 100))
-          
+
     # compare percentage to alert value, if greater or equal, then add 1 strike, else remove strikes
     if [[ ${CPU_USAGE_PERCENTILE} -ge ${ALERT_VALUE} ]]; then
         STRIKES=$((${STRIKES} + 1))
         CPU_HISTORY[${STRIKES}]=${CPU_USAGE_PERCENTILE}
     else
         STRIKES=0
-    fi  
-        
+    fi
+
     # if strikes are equal (or greater) than TIME_COUNT, append alarm to FILE
     if [[ ${STRIKES} -ge ${TIME_COUNT} ]]; then
         echo "$(date '+%Y-%m-%d %H:%M:%S') - ALERT! - CPU USAGE is above ${ALERT_VALUE}%, measured every ${TIME_INTERVAL} seconds. Values: ${CPU_HISTORY}" >> ${LOG_FILE}
+
+        # DEBUG
+        echo "CPU_USAGE = ${CPU_USAGE_PERCENTILE}%; STRIKES = ${STRIKES}"
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - ALERT! - CPU USAGE is above ${ALERT_VALUE}%, measured every ${TIME_INTERVAL} seconds. Values: ${CPU_HISTORY}"
+        # DEBUG END        
+
         # nullify strikes and wait AFTER_ALARM_SLEEP seconds, to avoid spam
         STRIKES=0
         sleep ${AFTER_ALARM_SLEEP}
-    fi      
+    fi
+
+    # DEBUG
+    #echo "CPU_USAGE = ${CPU_USAGE_PERCENTILE}%; STRIKES = ${STRIKES}"
+    # DEBUG END
 
     # wait a little while before measuring CPU USAGE again:
     sleep ${TIME_INTERVAL}
