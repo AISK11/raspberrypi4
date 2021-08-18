a214fca2820abaa9e832812076cab23c58fbe372
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-08-18 17:13:00 +0200

    Add files via upload

--- dotfiles/.config/i3/scripts/i3_prtsc.sh
+++ dotfiles/.config/i3/scripts/i3_prtsc.sh
@@ -4,18 +4,24 @@
 # Author: AISK11
 
 # Packages installed:
-# sudo apt install scrot
+# sudo apt install scrot xclip
 # Description: This script make png print screen in ${DIRECTORY} with time as a name.
 # Created for: ~/.config/i3/config:
 # bindsym Print exec ~/.config/i3/scripts/i3_prtsc.sh
 ##################################################################
 
 DIRECTORY="/home/$(whoami)/.screenshots/"
+FILENAME="$(date '+%Y-%m-%d_%H:%M:%S').png"
 
-if [ -d ${DIRECTORY} ]
-then
-    scrot --silent ${DIRECTORY}$(date '+%Y-%m-%d_%H:%M:%S').png
-else
-    mkdir -p ~/.screenshots/
-    scrot --silent ${DIRECTORY}$(date '+%Y-%m-%d_%H:%M:%S').png
+if [[ ! -d ${DIRECTORY} ]]; then
+    mkdir -p ~/.screenshots/ 
 fi
+
+if [[ "$1" = "-p" ]]; then
+    scrot --silent --pointer ${DIRECTORY}${FILENAME}
+else
+    scrot --silent ${DIRECTORY}${FILENAME}
+fi
+
+# select print screened image to clipboard:
+xclip -selection clipboard -target image/png -i ${DIRECTORY}${FILENAME} 
