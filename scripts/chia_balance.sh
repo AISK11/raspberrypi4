1d3f89a65552bd935ee568b2d7f8f9ce49039642
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-10-25 00:01:00 +0200

    Update chia_balance.sh

--- scripts/chia_balance.sh
+++ scripts/chia_balance.sh
@@ -15,8 +15,7 @@ USD_TO_EUR=$(curl -s 'https://wise.com/us/currency-converter/usd-to-eur-rate' | 
 if [[ -z ${BALANCE} ]] || [[ -z ${CHIA_PRICE} ]] || [[ -z ${USD_TO_EUR} ]]; then
     echo "Error!"
 else
-    echo "${BALANCE} * ${CHIA_PRICE} * ${USD_TO_EUR}"
+    #echo "${BALANCE} * ${CHIA_PRICE} * ${USD_TO_EUR}"
     EUR=$(python3 -c "print(${BALANCE} * ${CHIA_PRICE} * ${USD_TO_EUR})")
     echo "${EUR} EUR"
-    #echo " = $((BALANCE*CHIA_PRICE*USD_TO_EUR)) Eur."
 fi
